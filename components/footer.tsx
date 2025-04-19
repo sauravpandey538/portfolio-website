0c65aa3b8b4d7557dc694c6d4be0d78329c88066
sauravpandey538 <sauravpandey0325@icloud.com>
2025-04-19 21:27:47 +0545

    footer and css fixed

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; {currentYear} Sauurav. All rights reserved.
+        &copy; {currentYear} Saurav. All rights reserved.
       </small>
       <p className="text-xs">
         Let's connect and collaborate to create something amazing together!
--- components/project.tsx
+++ components/project.tsx
@@ -48,7 +48,7 @@ export default function Project({
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
-                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white/70 rounded-full dark:text-white/70"
                 key={index}
               >
                 {tag}
