e57fc21e8a71bc2d8c10f0b632ae45c977088150
sauravpandey538 <sauravpandey0325@icloud.com>
2025-04-25 08:54:52 +0545

    Update download link and text for resume in Intro component; add new resume file

--- components/intro.tsx
+++ components/intro.tsx
@@ -104,10 +104,10 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Saurav_Pandey_CV.pdf"
+          href="/Saurav_Pandey_Resume.pdf"
           download
         >
-          Download CV{" "}
+          Download Resume{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
